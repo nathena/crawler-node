34e7c92fc06f3a840009694e0485fbab63284db8
nathena@qq.com <nathena@qq.com>
2016-06-09 15:38:38 +0800

    fixed request cookiejar，forked python's cookielib add_cookie_header method.

--- crawlers/index.js
+++ crawlers/index.js
@@ -1,10 +1,10 @@
 
-require("./person/xmfish");
-require("./person/s917_xm");
-//require("./person/ganji");
-require("./person/anjuke");
-require("./person/fang");
-require("./person/baixing");
-require("./person/s58");
-//
-require("./second_hand_house/s58_shh");
+//require("./person/xmfish");
+//require("./person/s917_xm");
+require("./person/ganji");
+//require("./person/anjuke");
+//require("./person/fang");
+//require("./person/baixing");
+//require("./person/s58");
+
+//require("./second_hand_house/s58_shh");
